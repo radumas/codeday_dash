b7b369915026644d4c565f164ecf4e16c2cd6fd2
Raphael Dumas <raphael.a.dumas@gmail.com>
2017-08-14 16:36:14 -0400

    Link map and graph
    Graph displays on click of segment in map

--- graph_map.py
+++ graph_map.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 import json
-
+import itertools
 import plotly.graph_objs as go
 
 from psycopg2 import connect
@@ -23,35 +23,36 @@ app = dash.Dash()
 
 resultid = pgsql.Literal('BR2_BR3')
 
+resultids = pgsql.Literal(['BR1_BR2', 'BR2_BR3', 'BR3_BR4'])
 
 weekday_avg_sql = pgsql.SQL('''
-SELECT datetime_bin::TIME AS "Time", AVG(tt)
+WITH agg AS(SELECT segment_id, start_road ||': ' || start_crossstreet ||' to '||end_crossstreet as segment_name, 
+datetime_bin::TIME AS "Time", AVG(tt)::INT
 FROM bluetooth.aggr_5min
 INNER JOIN bluetooth.ref_segments USING (analysis_id)
-WHERE segment_name = {resultid} and EXTRACT('isodow' FROM datetime_bin) <6 AND datetime_bin >= '2017-01-01'
+WHERE ARRAY[segment_name::TEXT] <@ {resultids} and EXTRACT('isodow' FROM datetime_bin) <6 AND datetime_bin >= '2017-01-01'
 AND datetime_bin < '2017-02-01'
-GROUP BY "Time" 
-ORDER BY "Time" ''')
+GROUP BY segment_id, segment_name, "Time" )
 
-richmond_jan = pandasql.read_sql(weekday_avg_sql.format(resultid=resultid), con)
-
-trace = go.Scatter(x=richmond_jan['Time'],
-                   y=richmond_jan['avg'],
-                   mode='lines')
-layout = dict(title = "Average Weekday Travel Times",
-              xaxis = dict(title="Time of Day"),
-              yaxis = dict(title="Travel Time (s)"))
-
+SELECT * 
+FROM agg
+RIGHT OUTER JOIN (SELECT (generate_series(0,287) * interval '5 minutes')::TIME "Time") t using ("Time")
+ORDER BY segment_id, "Time"''')
+print('Loading Data')
+weekday_avg = pandasql.read_sql(weekday_avg_sql.format(resultids=resultids), con)
 
 
 mapbox_token = 'pk.eyJ1IjoicmVtb3RlZ2VudHJpZnkiLCJhIjoiY2lnanJzMjJpMDA1dnYxbHo5MTZtdGZsYSJ9.gLE8d40zmDAtMSSZyd2h1Q'
 
-geometry_sql = pgsql.SQL('''SELECT resultid, ST_ASGeoJSON(geom) geojson
-FROM gis.bluetooth_routes
---FROM bluetooth.routes
-WHERE resultid = {resultid} ''')
 
-richmond_df = pandasql.read_sql(geometry_sql.format(resultid=resultid), con)
+
+geometry_sql = pgsql.SQL('''SELECT resultid, segment_id, start_road as segment_name, ST_ASGeoJSON(geom) geojson
+FROM gis.bluetooth_routes
+INNER JOIN bluetooth.ref_segments ON resultid = segment_name
+--FROM bluetooth.routes
+WHERE ARRAY[resultid::TEXT] <@ {resultids} ''')
+
+map_data = pandasql.read_sql(geometry_sql.format(resultids=resultids), con)
 
 def get_lat_lon(geojson):
     lons, lats = [], []
@@ -62,18 +63,19 @@ def get_lat_lon(geojson):
 
 segments = []
 
-for row in richmond_df.itertuples():
+for row in map_data.itertuples():
     geojson = json.loads(row.geojson)
     lats, lons = get_lat_lon(geojson)
     segments.append(go.Scattermapbox(
         lat=lats,
         lon=lons,
         mode='lines',
-        hoverinfo='text',
+        hoverinfo='name',
+        name=row.segment_name,
+        customdata=list(itertools.repeat(row.segment_id, times=len(lats))),
+        text=row.segment_name,
         showlegend=False
     ))
-
-
     
 map_layout = go.Layout(
     title='Bluetooth Segments',
@@ -92,11 +94,48 @@ map_layout = go.Layout(
     )
 )    
 
+                                
 
-app.layout = html.Div(children=[html.Div(dcc.Graph(id='travel-time-graph',
-                                                   figure=dict(data=[trace], layout=layout))),
+
+app.layout = html.Div(children=[html.Div(html.H2('Click on a segment to view average travel times for it.')),
                                 html.Div(dcc.Graph(id='bluetooth-map', 
-                                                   figure=dict(data=segments, layout=map_layout)))])
+                                                   figure=dict(data=segments, layout=map_layout))),
+#                                html.Pre(id='click-data', style={'border': 'thin lightgrey solid'}),
+                                html.Div(dcc.Graph(id='travel-time-graph'))
+                                ])
+#@app.callback(
+#    Output('click-data', 'children'),
+#    [Input('bluetooth-map', 'clickData')])
+#def print_click_data(click_data):
+#    segment_id = 63
+#    if click_data is not None:
+#        segment_id = click_data['points'][0]['customdata']
+#    segment_name = weekday_avg[weekday_avg['segment_id'] ==  segment_id].iloc[[0]]['segment_name']
+#    return str(segment_id) +': ' + segment_name
+
+@app.callback(
+    Output('travel-time-graph', 'figure'),
+    [Input('bluetooth-map', 'clickData')])
+def update_graph(segment):
+    segment_id = None
+    if segment is None:
+        segment_id = 63
+    else:
+        segment_id = segment['points'][0]['customdata']
+                                
+    filtered_data = weekday_avg[weekday_avg['segment_id'] ==  segment_id]
+    
+    title = filtered_data.iloc[0]['segment_name']
+    
+    
+    data = [go.Scatter(x=filtered_data['Time'],
+                   y=filtered_data['avg'],
+                   mode='lines')]
+    layout = dict(title = 'Average Weekday Travel Times <br>' +title,
+                  xaxis = dict(title="Time of Day"),
+                  yaxis = dict(title="Travel Time (s)"))
+    
+    return {'data':data, 'layout':layout}
 
 if __name__ == '__main__':
     app.run_server(debug=True)
